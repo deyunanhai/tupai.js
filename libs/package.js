3dd8960f27622e32068daa67461fc4acbcf51939
bocelli.hu <bocelli.hu@gmail.com>
2013-11-05 13:49:47 +0900

    package lazy load

--- libs/package.js
+++ libs/package.js
@@ -63,6 +63,7 @@
     var mGlobalUtils = undefined;
     var mAutoLoad = true;
     var mCacheEnabled = true;
+
     function mergeClassByClassPath(packageObj, classPath) {
 
         if(!classPath) return;
@@ -73,55 +74,59 @@
         if(cp[className]) return;
 
         var classObj = findClass(nameArr)
-        if(!classObj) {
-
-            // add to waitQueue
-            if(!packageObj._waitQueue) {
-                packageObj._waitQueue = {};
-            }
-            packageObj._waitQueue[classPath]=undefined;
-
-            // check if loading
-            var arr = mLoadingQueue[classPath];
-            if(arr) {
-                arr.push(packageObj);
-                return;
-            } else {
-                mLoadingQueue[classPath] = arr = [];
-                arr.push(packageObj);
-            }
-
-            if(!mAutoLoad) return;
-            // add script to head
-            var s = document.createElement('script');
-            s.type = 'text/javascript';
-            s.async = true;
-            if (window.ActiveXObject) {
-                s.onreadystatechange = function() {
-                    if (s.readyState == 'complete' || s.readyState == 'loaded')  {
-                        didJSLoaded(classPath);
-                    }
-                };
-            } else {
-                s.onload = function(){
-                    didJSLoaded(classPath);
-                };
-            }
-            var src = mRemoteBaseUrl + classPath.split('.').join('/') + '.js';
-            if(!mCacheEnabled) {
-                src += '?' + Date.now();
-            }
-            s.src = src;
-            document.getElementsByTagName('head')[0].appendChild(s);
-
-            //throw new Error('cannot find package!' + classPath);
-        } else {
+        if(classObj && classObj.__is_fake !== 99999) {
             cp[className] = classObj;
+            return;
         }
+
+        // add to waitQueue
+        if(!packageObj._waitQueue) {
+            packageObj._waitQueue = {};
+        }
+        packageObj._waitQueue[classPath] = undefined;
+
+        // check if loading
+        var arr = mLoadingQueue[classPath];
+        if(arr) {
+            arr.push(packageObj);
+            return;
+        } else {
+            mLoadingQueue[classPath] = arr = [];
+            arr.push(packageObj);
+        }
+
+        if(classObj) {
+            // run fake object
+            classObj.checkAndRun();
+            return;
+        }
+        if(!mAutoLoad) return;
+        // add script to head
+        var s = document.createElement('script');
+        s.type = 'text/javascript';
+        s.async = true;
+        if (window.ActiveXObject) {
+            s.onreadystatechange = function() {
+                if (s.readyState == 'complete' || s.readyState == 'loaded')  {
+                    didJSLoaded(classPath);
+                }
+            };
+        } else {
+            s.onload = function(){
+                didJSLoaded(classPath);
+            };
+        }
+        var src = mRemoteBaseUrl + classPath.split('.').join('/') + '.js';
+        if(!mCacheEnabled) {
+            src += '?' + Date.now();
+        }
+        s.src = src;
+        console.log(src);
+        document.getElementsByTagName('head')[0].appendChild(s);
     }
 
     function didClassLoaded(classPath) {
-        //console.log('did class loaded ' + classPath);
+        console.log('did class loaded ' + classPath);
         var arr = mLoadingQueue[classPath];
         if(!arr) return;
         for(var i=0, n=arr.length; i<n; i++) {
@@ -138,10 +143,10 @@
 
     function didJSLoaded(classPath) {
         //console.log('did js loaded ' + classPath);
-        var classObj = findClass(classPath.split('.'));
-        if(!classObj) return;
+        /*var classObj = findClass(classPath.split('.'));
+        if(!classObj) return;*/
 
-        didClassLoaded(classPath);
+        //didClassLoaded(classPath);
     }
 
     var packageClass = createClass({
@@ -154,15 +159,26 @@
                 copy(this._classProvider, mGlobalUtils);
             }
             this._classObject = undefined;
-            this._packageName = packageName;
-            this._className = undefined;
             this._runQueue = [];
+
+            this._useList = undefined;
+            this._className = undefined;
+            this._packageName = packageName;
         },
         use: function(packageName) {
-            mergeClassByClassPath(this, packageName);
+            if(!this._useList) this._useList = [];
+            this._useList.push(packageName);
             return this;
         },
         checkAndRun: function() {
+            console.log('check ' + this._className);
+            if(this._useList) {
+                var list = this._useList;
+                for(var i=0, n=list.length; i<n; i++) {
+                    mergeClassByClassPath(this, list[i]);
+                }
+                delete this._useList;
+            }
             if(this._waitQueue) {
                 for(var name in this._waitQueue) {
                     return false;
@@ -180,27 +196,42 @@
         // define(className, callback);
         // define(callback);
         define: function(arg1, arg2) {
-            this._runQueue.push(function(This) {
-                if(typeof arg1 === 'string') {
-                    var className = arg1;
+            if(typeof arg1 === 'string') {
+                var className = arg1;
+                this._className = className;
+                this._runQueue.push(function(This) {
                     var callback = arg2;
                     var obj = ((typeof callback !== 'function') ? callback : callback(This._classProvider));
                     This._packageObj[className] = obj;
                     This._classProvider[className] = obj;
-                    This._className = className;
                     This._classObject = obj;
+                });
+                if(mLoadingQueue[this._packageName + '.' + className]) {
+                    // waiting for me?
+                    this.checkAndRun();
                 } else {
+                    // create fake object for extend
+                    var This = this;
+                    this._packageObj[className] = {
+                        __is_fake: 99999,
+                        checkAndRun: function() {
+                            This.checkAndRun();
+                        }
+                    };
+                }
+            } else {
+                if(!this._className) throw new Error('must define with name first.');
+                this._runQueue.push(function(This) {
                     var callback = arg1;
-                    if(!This._classObject) throw new Error('must define with name first.');
+                    if(!This._classObject) throw new Error('must define classObject first.');
                     var obj = ((typeof callback !== 'function') ?
                                callback :
                                callback.apply(This._classObject, [This._classProvider]));
                     if(obj) {
                         copy(This._classObject, obj, true);
                     }
-                }
-            });
-            this.checkAndRun();
+                });
+            }
             return this;
         },
         run: function(callback) {
