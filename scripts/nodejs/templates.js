365ff49f0fd9f939b89cddd0899a88868b0b3b28
bocelli.hu <bocelli.hu@gmail.com>
2014-02-03 12:40:09 +0900

    fix possible EventEmitter memory leak detected problem

--- scripts/nodejs/templates.js
+++ scripts/nodejs/templates.js
@@ -10,6 +10,7 @@ var temp = require('temp');
 function compileTemplate(url, output, packageName, options) {
 
     var data = fs.readFileSync(url);
+    console.log('      ' + url);
 
     var needMinify = true;
     if(options && options.minify === false) {
@@ -70,34 +71,47 @@ function compileTemplates(input, output, packageName, options) {
     if(!files) return;
 
     options = options || {};
-    options.list = options.list || [];
-    files.forEach(function(file) {
+
+    var processFile = function() {
+        var file = files.shift();
+        //console.log('process ' + file);
+        if(!file) {
+            options.end && options.end(0);
+            return;
+        }
         if(file.match(/^[a-zA-Z].*$/)) {
             var p = path.join(input, file);
             var stat = fs.statSync(p);
 
             //console.log(file);
+            var op = {
+                minify: options.minify,
+                onStdoutData: options.onStdoutData,
+                onStderrData: options.onStderrData,
+                end: function(code, url) {
+                    if(code != 0) {
+                        options.end && options.end(0);
+                        return;
+                    }
+                    processFile();
+                }
+            };
             if(stat.isDirectory()) {
                 var newPackageName = (packageName?(packageName+'.'+file):file);
-                compileTemplates(p, output, newPackageName, options);
+                compileTemplates(p, output, newPackageName, op);
             } else if(stat.isFile() && file.match(/\.html$/)) {
                 var name = file.replace(/\.html$/, '');
                 var newPackageName = (packageName?(packageName+'.'+name):name);
-                options.list[p]=true;
-                compileTemplate(p, output, newPackageName, {
-                    minify: options.minify,
-                    onStdoutData: options.onStdoutData,
-                    onStderrData: options.onStderrData,
-                    end: function(code, url) {
-                        delete options.list[url];
-                        if(Object.keys(options.list).length == 0) {
-                            options.end && options.end(code, url);
-                        }
-                    }
-                });
+                compileTemplate(p, output, newPackageName, op);
+            } else {
+                processFile();
             }
+        } else {
+            processFile();
         }
-    });
+    };
+
+    processFile();
 }
 
 exports.compileTemplate = function(input, output, packageName, options) {
