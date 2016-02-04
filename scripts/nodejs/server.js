d24cf32dcbedebd6fe1c7178b7f769e64cad1c2e
DQNEO <dqneoo@gmail.com>
2016-02-04 18:34:12 +0900

    call useProxyRequest after use '/'

--- scripts/nodejs/server.js
+++ scripts/nodejs/server.js
@@ -187,7 +187,6 @@ function configDebugMode(app) {
     var webDir = path.join(__dirname, '..', '..', 'releases', mConfig.web);
     //console.log(mConfig);
 
-    useProxyRequest(app);
     app.use('/__js', express.static(mConfig.sources));
     app.use('/__js', express.static(mConfig.genTemplates));
     app.use('/__js', express.static(mConfig.genConfigs));
@@ -217,6 +216,7 @@ function configDebugMode(app) {
         }
     });
 
+    useProxyRequest(app);
     var tupaiRootDir = path.join(__dirname, '..', '..');
     app.use('/__tupairoot', express.static(tupaiRootDir));
 
